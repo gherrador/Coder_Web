bf7cb74a1bfcda5c3d1060cee653dc7a00b9f2f6
Gustavo Herrador <herradorgustavo@gmail.com>
2021-10-22 22:29:15 -0300

    Entrega_SEO

---
+++ script.js
@@ -0,0 +1,30 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+        $("#buttonup").slideDown(350);
+        $("#buttonup").addClass('animate')
+    } else {
+        $("#buttonup").slideUp(350);
+    }
+});
+
+$("#buttonup i").on('click', function(e) {
+    e.preventDefault();
+    $("body,html").animate({
+        scrollTop: 0
+    }, 100);
+    return false;
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 200) {
+            document.getElementById('menu').classList.add('fixed-top');
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('menu').classList.remove('fixed-top');
+
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
